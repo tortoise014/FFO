8246a7958379ef6ac9dfd26d9ffd6bd3e66dc4bf
tortoise <2891138827@qq.com>
2025-03-21 19:54:40 +0800

    完成了

--- src/main/java/FFO/Main.java
+++ src/main/java/FFO/Main.java
@@ -1,5 +1,13 @@
 package FFO;
 
+import FFO.cli.CommandLineParser;
+import FFO.file.FileHandler;
+import FFO.generator.AnswerCalculator;
+import FFO.generator.ArithmeticGenerator;
+import FFO.grader.AnswerGrader;
+
+import java.util.List;
+
 /**
  * @Author tortoise
  * @Date 2025/3/11 0:29
@@ -9,4 +17,29 @@ package FFO;
  * @Version 1.0
  */
 public class Main {
+    public static void main(String[] args) {
+
+        CommandLineParser parser = new CommandLineParser(args);
+        if (parser.hasError()) {
+            System.out.println(parser.getHelpMessage());
+            return;
+        }
+
+        if (parser.isGenerateMode()) {
+            // 生成模式
+            List<String> questions = ArithmeticGenerator.generate(parser.getNumQuestions(), parser.getRange());
+            List<String> answers = AnswerCalculator.getAnswers(questions);
+
+            FileHandler.writeFile("Exercises.txt", questions);
+            FileHandler.writeFile("Answers.txt", answers);
+        } else if (parser.isGradeMode()) {
+            // 评分模式
+            List<String> questions = FileHandler.readFile(parser.getExerciseFile());
+            List<String> answers = FileHandler.readFile(parser.getAnswerFile());
+
+            AnswerGrader grader = new AnswerGrader();
+            grader.grade(questions, answers);
+        }
+    }
 }
+
--- src/main/java/FFO/grader/AnswerGrader.java
+++ src/main/java/FFO/grader/AnswerGrader.java
@@ -1,4 +1,9 @@
 package FFO.grader;
+import FFO.file.FileHandler;
+import FFO.generator.AnswerCalculator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author tortoise
@@ -9,4 +14,61 @@ package FFO.grader;
  * @Version 1.0
  */
 public class AnswerGrader {
+    /**
+     * 判定题目和答案的对错，并统计结果。
+     *
+     * @param questions 题目列表
+     * @param answers   答案列表
+     */
+    public void grade(List<String> questions, List<String> answers) {
+        List<Integer> correctIndices = new ArrayList<>(); // 正确的题目编号
+        List<Integer> wrongIndices = new ArrayList<>();   // 错误的题目编号
+        List<String> answers1 = AnswerCalculator.getAnswers(questions);
+        for (int i = 0; i < questions.size(); i++) {
+            String question = questions.get(i);
+            String expectedAnswer = answers.get(i);
+
+            // 计算题目的正确答案
+            // 比较答案
+            if (answers1.get(i).equals(expectedAnswer)) {
+                correctIndices.add(i + 1); // 题目编号从 1 开始
+            } else {
+                wrongIndices.add(i + 1);
+            }
+        }
+
+        // 输出统计结果
+        saveResult(correctIndices, wrongIndices);
+    }
+
+    /**
+     * 将统计结果保存到文件。
+     *
+     * @param correctIndices 正确的题目编号
+     * @param wrongIndices   错误的题目编号
+     */
+    private void saveResult(List<Integer> correctIndices, List<Integer> wrongIndices) {
+        StringBuilder result = new StringBuilder();
+        result.append("Correct: ").append(correctIndices.size()).append(" (");
+        for (int i = 0; i < correctIndices.size(); i++) {
+            result.append(correctIndices.get(i));
+            if (i < correctIndices.size() - 1) {
+                result.append(", ");
+            }
+        }
+        result.append(")\n");
+
+        result.append("Wrong: ").append(wrongIndices.size()).append(" (");
+        for (int i = 0; i < wrongIndices.size(); i++) {
+            result.append(wrongIndices.get(i));
+            if (i < wrongIndices.size() - 1) {
+                result.append(", ");
+            }
+        }
+        result.append(")");
+
+        // 写入文件
+        FileHandler.writeFile("Grade.txt", List.of(result.toString()));
+        System.out.println("统计结果已保存到 Grade.txt");
+    }
 }
